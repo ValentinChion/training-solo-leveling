c19afaca972682fa63e52ae4e96197ed61fac343
Valentin Chion <valentin.chion@consulting-for.accor.com>
2026-02-22 19:39:55 +0100

    Forgot data

--- src/features/player/data.ts
+++ src/features/player/data.ts
@@ -1,5 +1,5 @@
-import type { PlayerProfile } from "@/lib/orv/types";
-import { MOCK_PLAYER } from "@/lib/orv/mock-data";
+import { MOCK_PLAYER } from "@/features/player/mock";
+import { PlayerProfile } from "./types";
 
 // TODO: replace with real DB fetch
 export async function getPlayer(): Promise<PlayerProfile> {
